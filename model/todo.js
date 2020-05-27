b19378593ee7bdbead8d517e4361e8dda7d76c31
vd89 <me@daxcodes.in>
2020-05-27 11:35:41 +0530

    the data view on the view after submit

--- model/todo.js
+++ model/todo.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-  
+  item : String
 })
+
+module.exports = mongoose.model('todo',TodoSchema,'todos')
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const Todo = require('../model/todo')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/',async function(req, res, next) {
+  await Todo.find({},(err,data) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.render('index',{todos:data})
+    }
+  })
 });
+
+router.post('/submit', async (req,res) => {
+  // console.log(req.body);
+  const todo_item = new Todo(req.body)
+  console.log(todo_item);
+  try {
+    await todo_item.save()
+    await Todo.find({},(err,data) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.render('index',{todos:data})
+      }
+    })
+  } catch (error) {
+    res.send(error)
+  }
+})
 
 module.exports = router;
